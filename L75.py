3ec6c47f31f48a854fd58b285cfdc255d7ed83f9
Sebastian <97102748+IamdreamingaboutworkingforInteloneday@users.noreply.github.com>
2022-11-24 10:31:14 +0100

    Add files via upload

---
+++ L75.py
@@ -0,0 +1,63 @@
+MORSE = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+}
+
+
+def morse_decoder(code: str) -> str:
+    # replace this for solution
+    tablica_tx=[]
+    tablica=code.split("   ")
+    for i in tablica:
+        podtx=""
+        podtablica=i.split(" ")
+        for j in podtablica:
+            podtx+=MORSE[j]
+        tablica_tx.append(podtx)
+    return " ".join(tablica_tx).capitalize()
+
+
+print("Example:")
+print(morse_decoder("... --- -- .   - . -..- -"))
+
+assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+assert (
+    morse_decoder("..   .-- .- ...   -... --- .-. -.   .. -.   .---- ----. ----. -----")
+    == "I was born in 1990"
+)
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
